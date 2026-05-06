e194b7442ad46b81ef0286ffd0c67d3d97f1c31f
Victor Nani <123387218+AristarhMD@users.noreply.github.com>
2026-05-06 23:00:54 +0300

    Pre-final commit
    Added missing layout for desktop, and table.
    Logic of JS for switching radio inputs, calculate BMI and optimal body weight.

---
+++ script.js
@@ -0,0 +1,100 @@
+const formEl = document.querySelector("form");
+const resultEl = document.querySelector(".result");
+const metricContainerEl = document.querySelector(".metric-container");
+const imperialContainerEl = document.querySelector(".imperial-container");
+const measureTypeEl = document.querySelectorAll("input[name='measure']");
+
+formReset();
+
+formEl.addEventListener("input", (e) => {
+  e.preventDefault();
+
+  const rawData = new FormData(formEl);
+  const data = Object.fromEntries(rawData);
+
+  if (data.measure === "metric") {
+    if (data.height > 0 && data.weight > 0) {
+      const bmi = ((data.weight / (data.height * data.height)) * 10000).toFixed(
+        2,
+      );
+      const minWeight = (18.5 * Math.pow(data.height / 100, 2)).toFixed(2);
+      const maxWeight = (24.9 * Math.pow(data.height / 100, 2)).toFixed(2);
+
+      resultEl.innerHTML = `
+        <div class="md:grid md:grid-cols-2 md:gap-x-6">
+                <div class="mb-6 md:mb-0">
+                  <p
+                    class="text-preset-6 text-base leading-[150%] font-semibold text-white mb-2"
+                  >
+                    Your BMI is...
+                  </p>
+                  <span class="text-preset-2 text-white">${bmi}</span>
+                </div>
+                <p class="text-preset-7-regular text-white md:self-center">
+                  Your BMI suggests you're a ${Number(bmi) > 24.9 || Number(bmi) < 18.5 ? "unhealthy" : "healthy"} weight. Your Ideal wieght is
+                  between <span class="text-preset-7-bold">${minWeight}kgs - ${maxWeight}kgs</span>.
+                </p>
+              </div>`;
+    } else {
+      resultEl.innerHTML = `
+              <p class="text-preset-4 text-white mb-6">Welcome!</p>
+
+              <p class="text-preset-7-regular text-white">
+                Enter your height and weight and you’ll see your BMI result here
+              </p>`;
+    }
+  }
+
+  if (data.measure === "imperial") {
+    if (data.foot >= 0 && data.inch > 0 && data.stone >= 0 && data.pounds > 0) {
+      const totalIn = Math.pow(Number(data.foot) * 12 + Number(data.inch), 2);
+      const totalLbs = Number(data.stone) * 14 + Number(data.pounds);
+      const bmi = ((totalLbs / totalIn) * 703).toFixed(2);
+
+      const totalMinWeight = (18.5 * totalIn) / 703;
+      const minWeightSt = Math.trunc(totalMinWeight / 14);
+      const minWeightLbs = Math.trunc(totalMinWeight - minWeightSt * 14);
+
+      const totalMaxWeight = (24.9 * totalIn) / 703;
+      const maxWeightSt = Math.trunc(totalMaxWeight / 14);
+      const maxWeightLbs = Math.trunc(totalMaxWeight - maxWeightSt * 14);
+
+      resultEl.innerHTML = `
+        <div class="md:grid md:grid-cols-2 md:gap-x-6">
+                <div class="mb-6 md:mb-0">
+                  <p
+                    class="text-preset-6 text-base leading-[150%] font-semibold text-white mb-2"
+                  >
+                    Your BMI is...
+                  </p>
+                  <span class="text-preset-2 text-white">${bmi}</span>
+                </div>
+                <p class="text-preset-7-regular text-white md:self-center">
+                  Your BMI suggests you're a ${Number(bmi) > 24.9 || Number(bmi) < 18.5 ? "unhealthy" : "healthy"} weight. Your Ideal wieght is
+                  between <span class="text-preset-7-bold">${minWeightSt}st ${minWeightLbs}lbs - ${maxWeightSt}st ${maxWeightLbs}lbs</span>.
+                </p>
+              </div>`;
+    } else {
+      resultEl.innerHTML = `
+              <p class="text-preset-4 text-white mb-6">Welcome!</p>
+
+              <p class="text-preset-7-regular text-white">
+                Enter your height and weight and you’ll see your BMI result here
+              </p>`;
+    }
+  }
+});
+
+measureTypeEl.forEach((measure) => {
+  measure.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      metricContainerEl.classList.toggle("hidden");
+      imperialContainerEl.classList.toggle("hidden");
+    }
+  });
+});
+
+function formReset() {
+  formEl.reset();
+  measureTypeEl[0].checked;
+}
